e5469213e975ad2a63cbd651a5ed7ce04b48c7f0
Rohit Damkondwar <rohitsdamkondwar@gmail.com>
2017-10-19 19:23:47 -0500

    Fixing printing on console

---
+++ src/main/scala/ConsoleSink2.scala
@@ -0,0 +1,37 @@
+import org.apache.spark.sql.execution.streaming.Sink
+import org.apache.spark.sql.sources.StreamSinkProvider
+import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.{DataFrame, SQLContext}
+
+class CustomConsoleSink() extends StreamSinkProvider {
+
+  // Track the batch id
+  private var lastBatchId = -1L
+
+  def createSink(
+                  sqlContext: SQLContext,
+                  parameters: Map[String, String],
+                  partitionColumns: Seq[String],
+                  outputMode: OutputMode): Sink = {
+    new Sink {
+      override def addBatch(batchId: Long, data: DataFrame): Unit = synchronized {
+        val batchIdStr = if (batchId <= lastBatchId) {
+          s"Rerun batch: $batchId"
+        } else {
+          lastBatchId = batchId
+          s"Batch: $batchId"
+        }
+
+        // scalastyle:off println
+        println("-------------------------------------------")
+        println(batchIdStr)
+        println("-------------------------------------------")
+        // scalastyle:off println
+        data.sparkSession.createDataFrame(
+          data.sparkSession.sparkContext.parallelize(data.collect()), data.schema)
+          .collect().foreach(println)
+      }
+    }
+  }
+}
+
--- src/main/scala/PartAQuestion1.scala
+++ src/main/scala/PartAQuestion1.scala
@@ -1,22 +1,24 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.StructType
 
 object PartAQuestion1 {
    def main(args: Array[String]) {
    val dirPath: String = args(0)
    val spark = SparkSession.builder.appName("PartAQuestion1")
-   .config("spark.eventLog.enabled", "true")
-   .config("spark.eventLog.dir", "hdfs://10.254.0.146/spark/history")
+   //.config("spark.eventLog.enabled", "true")
+   //.config("spark.eventLog.dir", "hdfs://10.254.0.146/spark/history")
    .getOrCreate()
+
+   spark.sparkContext.setLogLevel("ERROR")
    import spark.implicits._
 
    val userSchema = new StructType().add("userA", "long").add("userB", "long").add("timestamp", "timestamp").add("action", "string")
 
    val csvDF = spark.readStream.option("sep", ",").schema(userSchema).csv(dirPath)
 
-   val wordCounts = csvDF.groupBy(window($"timestamp", "60 minutes", "30 minutes"),$"action").count()
-   val query = wordCounts.writeStream.outputMode("complete").format("console").option("numRows", 100).option("truncate", false).start()
+   val wordCounts = csvDF.groupBy(window($"timestamp", "60 minutes", "30 minutes"),$"action").count().orderBy($"window", $"action")
+   val query = wordCounts.writeStream.outputMode("complete").format(classOf[CustomConsoleSink].getCanonicalName).option("checkpointLocation", false).start()
    query.awaitTermination()
    }
 }
